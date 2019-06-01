9846315c7406882e6b86eb5887bbdf5bd666e5ee
davidh <iq.davidh@gmail.com>
2019-06-01 11:12:11 -0500

    funcionando canvas, sigue pintar fondo

--- src/app.js
+++ src/app.js
@@ -7,12 +7,19 @@
 let game= {
 
     start:function(){
+        this.canvas=document.createElement('canvas');
+        this.canvas.width = 800;
+        this.canvas.height = 600;
 
 
-
+        this.body=document.getElementById('body');
+        this.body.append(this.canvas);
     },
     canvas:null,
-    ctx:null
+    ctx:null,
+    getBody:function(){
+        return this.body;
+    }
 
 };
 
