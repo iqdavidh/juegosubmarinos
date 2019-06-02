70dfc8daba5d6f01bce73f891b5cb5a9f36926ad
David H <iq.davidh@gmail.com>
2019-06-02 17:11:55 -0500

    recatorizado tablero faltan submarinos

--- public/src/app.js
+++ public/src/app.js
@@ -398,6 +398,47 @@ const drawEtapaSeleccionarPosicion = {
         const sizeRegion = gameConfig.size / 3;
         const delta = gameConfig.deltaSep;
 
+
+
+        let cacheRegionConMar=this.getCacheCanvasRegionConMar( jugador);
+
+        let origen = getOriginFromIndex(jugador.indexCuadrante);
+        let origenMar = new Posicion(origen.x + delta, origen.y + delta);
+        let sizeMar = sizeRegion - 2 * delta;
+
+        /* draw el cache  */
+        ctx.drawImage(cacheRegionConMar, 0, 0, sizeRegion, sizeRegion, origen.x, origen.y, sizeRegion, sizeRegion);
+
+        //ctxCache.fillText('COHETES LISTOS', delta + 24, sizeMar + delta + 18);
+
+        /* actualziar dra numero coehtes*/
+        // let numCoheteListo = jugador.getNumCohetesReady();
+        //
+        // ctx.font = '20px monospace';
+        // ctx.fillStyle = "rgba(0, 255, 0, 1)";
+        // ctx.fillText(numCoheteListo.toString(), origenMar.x, origenMar.y + sizeMar + 18);
+
+
+        /* actualziar dra numero sub*/
+        let numSubmarino = jugador.getNumSubmarinos();
+        ctx.fillStyle = "rgba(255, 255, 0, 1)";
+        ctx.font = '20px monospace';
+        ctx.fillText(numSubmarino.toString(), origen.x + sizeRegion - delta - 20, origen.y + delta - 6);
+
+
+    },
+    cacheCanvasRegionConMar:null,
+
+    getCacheCanvasRegionConMar:function(jugador  ){
+
+        if(this.cacheCanvasRegionConMar !==null){
+            return this.cacheCanvasRegionConMar;
+        }
+
+        const sizeRegion = gameConfig.size / 3;
+        const delta = gameConfig.deltaSep;
+
+
         const cacheRegionConMar = document.createElement('canvas');
         cacheRegionConMar.width = sizeRegion;
         cacheRegionConMar.height = sizeRegion;
@@ -410,7 +451,6 @@ const drawEtapaSeleccionarPosicion = {
 
         //la seccion de mar
         let sizeMar = sizeRegion - 2 * delta;
-        let origenMar = new Posicion(origen.x + delta, origen.y + delta);
         ctxCache.drawImage(gameConfig.resources.imgMar, 0, 0, sizeMar, sizeMar, delta, delta, sizeMar, sizeMar);
 
         //las divisiones
@@ -430,30 +470,13 @@ const drawEtapaSeleccionarPosicion = {
         /* el cache de texto */
         ctxCache.font = '20px monospace';
         ctxCache.fillStyle = "rgba(200, 200, 200, 0.7)";
-        ctxCache.fillText('COHETES LISTOS', delta + 24, sizeMar + delta + 18);
         ctxCache.fillText('SUBMARINOS', delta + 70, 24);
 
+        this.cacheCanvasRegionConMar=cacheRegionConMar;
 
-        /* draw el cache  */
-        ctx.drawImage(cacheRegionConMar, 0, 0, sizeRegion, sizeRegion, origen.x, origen.y, sizeRegion, sizeRegion);
-
-
-        /* actualziar dra numero coehtes*/
-        let numCoheteListo = jugador.getNumCohetesReady();
-
-        ctx.font = '20px monospace';
-        ctx.fillStyle = "rgba(0, 255, 0, 1)";
-        ctx.fillText(numCoheteListo.toString(), origenMar.x, origenMar.y + sizeMar + 18);
-
-
-        /* actualziar dra numero sub*/
-        let numSubmarino = jugador.getNumSubmarinos();
-        ctx.fillStyle = "rgba(255, 255, 0, 1)";
-        ctx.fillText(numSubmarino.toString(), origen.x + sizeRegion - delta - 20, origen.y + delta - 6);
-
+        return cacheRegionConMar;
 
     }
-
 };
 
 
@@ -468,6 +491,6 @@ function getOriginFromIndex(index) {
     }
 
 }
-/*FBUILD*/ console.log( 'FBUILD-20190602 16:24');  /*FBUILD*/
+/*FBUILD*/ console.log( 'FBUILD-20190602 17:11');  /*FBUILD*/
 
 //# sourceMappingURL=app.js.map
