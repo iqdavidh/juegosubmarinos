11d878d91f81f695180abf0b9bc9483d185e806e
David H <iq.davidh@gmail.com>
2019-06-08 00:17:48 -0500

    ya tengo la rotacion del cohete

--- src/03EtapaBatalla/FactoryImgRocket.js
+++ src/03EtapaBatalla/FactoryImgRocket.js
@@ -19,27 +19,74 @@ const factoryImgRocket = {
             return this.cache1;
         }
 
-        //crear cache solicitado
+        const canvasFrame = document.createElement('canvas');
+        canvasFrame.width = size;
+        canvasFrame.height = size;
+        const ctxFrame = canvasFrame.getContext('2d');
+
+
+        const wResource = 75;
+        const hResource = 19;
+        const alfa = size / wResource;
+
+
+        let y = (size - hResource) / 2;
+
+
+        //crear cache de todos amgulos -------------------------
         const canvasCache = document.createElement('canvas');
-        canvasCache.width = size * 360;
+        canvasCache.width = size * 359;
         canvasCache.height = size;
 
-        const ctx=canvasCache.getContext('2d');
-
-        const wResource=75;
-        const hResource=19;
-        const alfa= size/wResource;
-
-        //es el cohete rotado en todos los ángulos
-        for (let i = 0; i < 360; i++) {
-            let y = (size-hResource)/2;
-            ctx.drawImage( gameConfig.resources.imgRocket, 0, 0,
-                wResource , hResource, i* size, y, size, hResource * alfa );
-
-        }
+        const ctx = canvasCache.getContext('2d');
 
 
-        this.cache1=canvasCache;
+        ctxFrame.drawImage(gameConfig.resources.imgRocket, 0, 0,
+            wResource, hResource, 0, y, size, hResource * alfa);
+
+        const canvasRot = document.createElement('canvas');
+        canvasRot.width = size;
+        canvasRot.height = size;
+        const ctxRot = canvasRot.getContext('2d');
+
+
+        ctxRot.save();
+        ctxRot.translate(size/2,size/2);
+        ctxRot.rotate(-90*Math.PI/180);
+        ctxRot.drawImage(canvasFrame, -(size/2), -(size/2));
+
+
+        ctx.drawImage(canvasRot, 0, 0,
+            size, size, 0 * size, 0, size, size);
+
+
+        // //es el cohete rotado en todos los ángulos
+        // for (let i = 0; i < 360; i++) {
+        //
+        //     ctxFrame.clearRect(0,0,size,size);
+        //
+        //     if( i%2){
+        //         ctxFrame.fillStyle="#ffffff";
+        //         ctxFrame.fillRect(0,0,size,size);
+        //     }
+        //
+        //
+        //     ctxFrame.drawImage(gameConfig.resources.imgRocket, 0, 0,
+        //         wResource, hResource, 0, y, size, hResource * alfa);
+        //
+        //
+        //     ctxFrame.translate(size/2, size/2);
+        //     ctxFrame.rotate(90*Math.PI/180);
+        //
+        //     //dibujar el cohete en el frame ----------------------
+        //     ctx.drawImage(canvasFrame, 0, 0,
+        //         size, size, i * size, 0, size, size);
+        //
+        //
+        // }
+
+
+        this.cache1 = canvasCache;
 
         return this.cache1;
 
@@ -57,24 +104,24 @@ const factoryImgRocket = {
         canvasCache.width = size * 360;
         canvasCache.height = size;
 
-        const ctx=canvasCache.getContext('2d');
+        const ctx = canvasCache.getContext('2d');
 
 
-        const wResource=75;
-        const hResource=19;
+        const wResource = 75;
+        const hResource = 19;
 
-        const alfa= size/wResource;
+        const alfa = size / wResource;
 
         //es el cohete rotado en todos los ángulos
         for (let i = 0; i < 360; i++) {
-            let y = (size-hResource)/2;
-            ctx.drawImage( gameConfig.resources.imgRocket, wResource, 0,
-                wResource , hResource, i* size, y, size, hResource * alfa );
+            let y = (size - hResource) / 2;
+            ctx.drawImage(gameConfig.resources.imgRocket, wResource, 0,
+                wResource, hResource, i * size, y, size, hResource * alfa);
 
         }
 
 
-        this.cache2=canvasCache;
+        this.cache2 = canvasCache;
 
         return this.cache2;
 
